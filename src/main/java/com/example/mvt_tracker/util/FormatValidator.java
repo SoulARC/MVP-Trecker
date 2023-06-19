f66afbe0204d0abb59d3c90844d199c656a8bd2e
Anatolii Bespalko <tolyan9901@gmail.com>
2023-06-19 21:53:18 +0300

    Fix

--- src/main/java/com/example/mvt_tracker/util/FormatValidator.java
+++
@@ -1,9 +0,0 @@
-package com.example.mvt_tracker.util;
-
-import com.example.mvt_tracker.exception.IncorrectFormatException;
-
-public interface FormatValidator {
-    boolean isValidBasketballFormat(String[] fields) throws IncorrectFormatException;
-
-    boolean isValidHandballFormat(String[] fields) throws IncorrectFormatException;
-}

edb7a86e0da59b79dbf18d9ed7b6d79333be06b7
Anatolii Bespalko <tolyan9901@gmail.com>
2023-06-19 17:53:04 +0300

    Refactored

--- src/main/java/com/example/mvt_tracker/service/enums/Games.java
+++ src/main/java/com/example/mvt_tracker/service/enums/Games.java
@@ -1,21 +1,6 @@
 package com.example.mvt_tracker.service.enums;
 
 public enum Games {
-    BASKETBALL("BASKETBALL"),
-    HANDBALL("HANDBALL");
-
-    private final String str;
-
-    Games(String str) {
-        this.str = str;
-    }
-
-    public boolean equals(String str) {
-        return this.str.equals(str);
-    }
-
-    @Override
-    public String toString() {
-        return str;
-    }
+    BASKETBALL,
+    HANDBALL
 }

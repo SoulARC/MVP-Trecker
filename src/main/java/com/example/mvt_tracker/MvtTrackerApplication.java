9922227f8fe09bc5c185e00f6ec7fd4817d3175a
Anatolii Bespalko <tolyan9901@gmail.com>
2023-06-19 17:53:57 +0300

    Run my project

--- src/main/java/com/example/mvt_tracker/MvtTrackerApplication.java
+++ src/main/java/com/example/mvt_tracker/MvtTrackerApplication.java
@@ -1,11 +1,43 @@
 package com.example.mvt_tracker;
 
+import com.example.mvt_tracker.model.Player;
+import com.example.mvt_tracker.service.GameResultProcessor;
+import com.example.mvt_tracker.service.MVPService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvtTrackerApplication {
+public class MvtTrackerApplication implements CommandLineRunner {
+    private final GameResultProcessor gameResultProcessor;
+    private final MVPService mvpService;
+    private static final String HANDBALL_PATH = "hball.csv";
+    private static final String BASKETBALL_PATH = "bball.csv";
+
+    public MvtTrackerApplication(GameResultProcessor gameResultProcessor, MVPService mvpService) {
+        this.gameResultProcessor = gameResultProcessor;
+        this.mvpService = mvpService;
+    }
     public static void main(String[] args) {
         SpringApplication.run(MvtTrackerApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        gameResultProcessor.gameStrategy(HANDBALL_PATH);
+        gameResultProcessor.gameStrategy(BASKETBALL_PATH);
+
+        Player mvp = mvpService.getMVP();
+        System.out.println("MVP: " + mvp.getNickname());
+        System.out.println("SCORE: " + mvp.getRatingPoints());
+    }
 }
+
+
+
+
+
+
+
+
+
